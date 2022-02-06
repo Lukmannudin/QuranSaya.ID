c0f25913041a09b2678a2bcd1a499a38b82b3e81
lukmannudin <lukmannudin.priatna@happyfresh.com>
2022-02-07 01:41:19 +0700

    update apiservice

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,9 +4,9 @@
 extern "C"
 jstring
 Java_com_oleg_quransayaid_common_Keys_getBaseUrlToken(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-        //https://quran-service.herokuapp.com
-        std::string baseUrl = "aHR0cHM6Ly9xdXJhbi1zZXJ2aWNlLmhlcm9rdWFwcC5jb20=";
-        return env->NewStringUTF(baseUrl.c_str());
-        }
+    //https://quran-service.herokuapp.com
+    std::string baseUrl = "aHR0cHM6Ly9xdXJhbi1zZXJ2aWNlLmhlcm9rdWFwcC5jb20=";
+    return env->NewStringUTF(baseUrl.c_str());
+}
